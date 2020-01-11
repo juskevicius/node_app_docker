bdd4c5890526d3f37b00de991e11207266cb2407
mantas_juskevicius <mantas.juskevicius@zenitech.co.uk>
2020-01-11 23:57:32 +0200

    added some tests

---
+++ tests/sample.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Get endpoint', () => {
+  it('Should test that true === true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Should return the webpage', async () => {
+    const res = await request(app)
+      .get('/');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toContain('Super duper webpage');
+  });
+});
